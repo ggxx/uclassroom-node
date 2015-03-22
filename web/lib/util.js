1eb01f7253a88d1b1f546085e9c32a32990f932c
ggxx <ggxx120@gmail.com>
2015-03-22 08:14:36 +0800

    change ssh-keygen parameters
    Signed-off-by: ggxx <ggxx120@gmail.com>

--- web/lib/util.js
+++ web/lib/util.js
@@ -19,48 +19,48 @@ function _isEmpty(obj) {
 };
 
 function s4() {
-	return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
 
 function _randomId() {
-	return s4() + s4();
+    return s4() + s4();
 }
 
 function _guid() {
-	return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
-	//return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+    //return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
 function _genSSHKey(email, callback) {
-	fs.exists('/tmp/uclassroom', function(exists) {
-		if (!exists) {
-			fs.mkdirSync('/tmp/uclassroom');
-		}
-		fs.exists('/tmp/uclassroom/ssh', function(exists) {
-			if (!exists) {
-				fs.mkdirSync('/tmp/uclassroom/ssh');
-			}
-			var tmp = '/tmp/uclassroom/ssh/' + _guid();
-			var cmd_keygen = 'ssh-keygen -t RSA -C "' + email + '" -f ' + tmp + ' -P "" ';
-			console.log('[exec] '.yellow + cmd_keygen.yellow);
-			process.exec(cmd_keygen, function (error, stdout, stderr) {
-				if (error !== null) {
-					console.log('exec error: ' + error);
-				}
-				else {
-					var pri = fs.readFileSync(tmp).toString();
-					var pub = fs.readFileSync(tmp + '.pub').toString();
-					callback(pri, pub);
-				}
-			});
-		});
-	});
+    fs.exists('/tmp/uclassroom', function (exists) {
+        if (!exists) {
+            fs.mkdirSync('/tmp/uclassroom');
+        }
+        fs.exists('/tmp/uclassroom/ssh', function (exists) {
+            if (!exists) {
+                fs.mkdirSync('/tmp/uclassroom/ssh');
+            }
+            var tmp = '/tmp/uclassroom/ssh/' + _guid();
+            var cmd_keygen = 'ssh-keygen -t RSA -b 2048 -C "' + email + '" -f ' + tmp + ' -q -N "" ';
+            console.log('[exec] '.yellow + cmd_keygen.yellow);
+            process.exec(cmd_keygen, function (error, stdout, stderr) {
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+                else {
+                    var pri = fs.readFileSync(tmp).toString();
+                    var pub = fs.readFileSync(tmp + '.pub').toString();
+                    callback(pri, pub);
+                }
+            });
+        });
+    });
 }
 
 function _getTime() {
-	var date = new Date();
-	return date.toLocaleTimeString();
-	//return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    var date = new Date();
+    return date.toLocaleTimeString();
+    //return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
 
