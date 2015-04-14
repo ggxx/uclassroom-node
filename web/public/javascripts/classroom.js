dd3bf6678812e2bf251cc7816172c8fcec8b86c0
ggxx <ggxx120@gmail.com>
2015-04-14 14:26:43 +0800

    add turn servers

--- web/public/javascripts/classroom.js
+++ web/public/javascripts/classroom.js
@@ -7,9 +7,30 @@ var mediaStream, screenStream;
 var remoteCameraStream, remoteScreenStream;
 var rtcPeerConnections = {};
 
-var iceServers = (navigator.mozGetUserMedia) ?
-{iceServers: [{url: 'stun:23.21.150.121'}]} : // number IP
-{iceServers: [{url: 'stun:stun.l.google.com:19302'}]};
+var iceServers = {
+    iceServers: [
+        {
+            url: 'stun:23.21.150.121'
+        },
+        {
+            url: 'stun:stun.l.google.com:19302'
+        },
+        {
+        	url: 'turn:numb.viagenie.ca',
+        	credential: 'muazkh',
+        	username: 'webrtc@live.com'
+        },
+        {
+        	url: 'turn:192.158.29.39:3478?transport=udp',
+        	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        	username: '28224511:1379330808'
+        },
+        {
+        	url: 'turn:192.158.29.39:3478?transport=tcp',
+        	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        	username: '28224511:1379330808'
+        }
+    ]};
 var optionalRtpDataChannels = {
     optional: [
         {RtpDataChannels: true},
